98e80bc754edb3475d2a07e6f5e8c09a6dbf3102
Oscar Guerrero <oscard.guerrerom@outlook.com>
2026-02-01 22:33:11 -0500

    Projects and testimonies sections created

---
+++ src/lib/testimonies.ts
@@ -0,0 +1,36 @@
+import { type Testimonies } from "@/types/testimonies"; 
+
+export const testimonies: Testimonies[] = [
+    {
+        id: 1,
+        comment: "This is a comment of a customer",
+        client_name: "Tatiana Rodríguez",
+        client: "CEO - Café Abuela Elina",
+        avatar: "./avataaars.svg",
+        rate: 4
+    },
+    {
+        id: 2,
+        comment: "This is another comment of a customer",
+        client_name: "Paola Ocampo",
+        client: "Uñas con Estilo",
+        avatar: "./avataaars_2.svg",
+        rate: 5
+    },
+    {
+        id: 3,
+        comment: "This is another comment of a customer",
+        client_name: "Jessica Calvo",
+        client: "Jessica Studio",
+        avatar: "./avataaars_3.svg",
+        rate: 4
+    },
+    {
+        id: 4,
+        comment: "This is another comment of a customer",
+        client_name: "Karen Jeréz",
+        client: "Karen Jeréz",
+        avatar: "./avataaars_4.svg",
+        rate: 5
+    }
+]
---
+++ src/types/testimonies.ts
@@ -0,0 +1,8 @@
+export type Testimonies = {
+    id: number,
+    comment: string,
+    client_name: string,
+    client: string,
+    avatar: string,
+    rate: number,
+}
