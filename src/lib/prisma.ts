86d99062d49e7772ced620f6702104cb65b668b3
Oscar Guerrero <oscard.guerrerom@outlook.com>
2026-02-12 00:50:33 -0500

    Updated prisma files and erased LeadsForm from fronted

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,12 @@
 import "dotenv/config";
-//import { createRequire } from "module";
-//import { PrismaPg } from "@prisma/adapter-pg";
-
-//const require = createRequire(import.meta.url);
-import { PrismaClient } from "../../prisma/generated/client";
-
-//const connectionString = process.env.PRISMA_DATABASE_URL;
-
-//if (!connectionString) {
-  //throw new Error("PRISMA_DATABASE_URL is not set");
-//}
-
-//const adapter = new PrismaPg({ connectionString });
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: InstanceType<typeof PrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient(); //{adapter,}
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
