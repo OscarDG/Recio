45b9e0ede0f5a865656913e3830d60d1bccf9460
Oscar Guerrero <oscard.guerrerom@outlook.com>
2026-02-11 20:40:38 -0500

    Refactor Prisma client initialization and update import path in leads API

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,12 @@
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../prisma/generated/client';
+import { PrismaClient } from "../../prisma/generated"
 
-const connectionString = import.meta.env.PRISMA_DATABASE_URL;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-export default prisma;
+export const prisma =
+    globalForPrisma.prisma ?? new PrismaClient({});
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+
