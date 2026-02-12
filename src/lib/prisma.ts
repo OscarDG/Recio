c84c7260442fcf47f2aedf3bdfd25eac540da5ce
Oscar Guerrero <oscard.guerrerom@outlook.com>
2026-02-11 22:18:51 -0500

    Update Prisma schema and connection handling for improved configuration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,23 @@
 import { createRequire } from "module";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const require = createRequire(import.meta.url);
 const { PrismaClient } = require("@prisma/client");
+
+const connectionString = process.env.PRISMA_DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: InstanceType<typeof PrismaClient> | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({});
+  globalForPrisma.prisma ?? new PrismaClient({adapter,});
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
